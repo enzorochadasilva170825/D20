f5e9633933f6da1adbf0875641d7e882751b5fc5
Enzo Rocha da Silva <enzoba.selva@gmail.com>
2025-09-03 17:32:48 -0300

    Update script.js

--- bolacha de leite condensado/script.js
+++ bolacha de leite condensado/script.js
@@ -1,57 +1,99 @@
-const forcaAdd = document.querySelector("#forca_add");
-const forcaSub = document.querySelector("#forca_sub");
+let forca = document.getElementById("forBase");
+let destreza = document.getElementById("desBase");
+let constituicao = document.getElementById("conBase");
+let inteligencia = document.getElementById("inBase");
+let sabedoria = document.getElementById("sabBase");
+let carisma = document.getElementById("carBase");
 
-const destrezaAdd = document.querySelector("#destreza_add");
-const destrezaSub = document.querySelector("#destreza_sub");
 
-const sabedoriaAdd = document.querySelector("#sabedoria_add");
-const sabedoriaSub = document.querySelector("#sabedoria_sub");
+let forD20 = document.getElementById("forD20")
+let desD20 = document.getElementById("desD20")
+let conD20 = document.getElementById("conD20")
+let intD20 = document.getElementById("intD20")
+let sabD20 = document.getElementById("sabD20")
+let carD20 = document.getElementById("carD20")
 
-const dadoRolar = document.querySelector("#dado_rolar");
 
-let valorForca = 0;
-let valorDestreza = 0;
-let valorSabedoria = 0;
-let valorDado = 0;
+let forMod = document.getElementById("forMod")
+let desMod = document.getElementById("desMod")
+let conMod = document.getElementById("conMod")
+let intMod = document.getElementById("intMod")
+let sabMod = document.getElementById("sabMod")
+let carMod = document.getElementById("carMod")
 
-function somar() {
-    const resultado =  document.querySelector("#resultado_valor");
-    let somaTotal = valorForca + valorDestreza + valorSabedoria + valorDado;
-    resultado.innerHTML = somaTotal;
+
+let totalForca = document.getElementById('forTotal')
+let totalDestreza = document.getElementById('desTotal')
+let totalConstituicao = document.getElementById('conTotal')
+let totalInteligencia = document.getElementById('intTotal')
+let totalSabedoria = document.getElementById('sabTotal')
+let totalCarisma = document.getElementById('carTotal')
+
+
+let botaoDado = document.getElementById("dado")
+let Calcularbotao = document.getElementById("calcular")
+let Resetarbotao = document.getElementById("resetar")
+
+calcularbotao.addEventListener('click', calcular);
+resetarbotao.addEventListener('click', resetar);
+botaoDado.addEventListener('click', camposD20);
+
+function camposD20(){
+    let array_numeros_aleatorios = [];
+
+    for (let i = 0; i < 6; i++) {
+        array_numeros_aleatorios[i] = Math.floor(20 * Math.random() + 1)
+    }
+    return array_numeros_aleatorios;
 }
 
-function rolarDado() {
-    valorDado = Math.floor(Math.rondom() * 20) +1;
+function camposD20() {
+    numero_d20 = gerarNumeroAleatorio();
 
-    const dadoValor = document.querySelector("#dado_valor");
-    dadoValor.innerHTML = valorDado;
+    forD20.value = numero_D20[0]
+    desD20.value = numero_D20[1]
+    conD20.value = numero_D20[2]
+    intD20.value = numero_D20[3]
+    sabD20.value = numero_D20[4]
+    carD20.value = numero_D20[5]
 
-    somar();
-    
+    modificador(numeros_d20);
 }
 
-dadoRolar.addEventListener("click", rolarDado);
-forcaAdd.addEventListener("click", ()=>{
-    valorForca++
-    document.querySelector("#forca_valor").value = valorForca;
-    somar();
-});
+function modificador(numeros_aleatorios) {
+    numeros_d20 = numeros_aleatorios;
+    let mod = []
 
-destrezaAdd.addEventListener("click", () => {
-    valorDestreza++;
-    document.querySelector("#Destreza_valor").value = valorDestreza
+    for (let i = 0; i < 6; i++) {
+        valor = numeros_d20[i];
+        if (valor > 15) {
+            mod[i] = Math.round(1 + (valor / 1.2));
+        } else if (valor > 7) {
+            mod[i] = (0 + (valor / 1.2)).toFixed(0);
+        }   else{
+            mod[i] = Math.round(-1 + (valor / 0.8));
+        }
+    }
 
-    somar();
-});
+    forMod.value = mod[0];
+    desMod.value = mod[1];
+    conMod.value = mod[2];
+    intMod.value = mod[3];
+    sabMod.value = mod[4];
+    carMod.value = mod[5];
+}
 
-destrezaSub.addEventListener("click", () => {
-    valorDestreza--;
-    document.querySelector("#destreza_valor").value = valorDestreza
-    somar();
-});
 
-sabedoriaAdd.addEventListener("click", () => {
-    valorSabedoria++;
-    document.querySelector("#sabedoria_valor").value = valorSabedoria;
-    somar();
-});
+function calcular()
+{
+    totalForca.value = Number(forca.value) + Number(forMod.value);
+    totalForca.value = Number(destreza.value) + Number(desMod.value);
+    totalForca.value = Number(constituicao.value) + Number(conMod.value);
+    totalForca.value = Number(inteligencia.value) + Number(intMod.value);
+    totalForca.value = Number(sabedoria.value) + Number(sabMod.value);
+    totalForca.value = Number(carisma.value) + Number(carMod.value);
+}
+
+function recarregarPagina() {
+    window.location.reload(true)
+}
